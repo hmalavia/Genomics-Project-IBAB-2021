131f2729c4ca097155f4dd508e753049cc7d3268
hmalavia <hmalavia2@gmail.com>
2021-12-26 20:30:42 +0530

    modification to script

--- Gkountela2019/scran_results/pathway_analysis/Br_11/Up/combine_files.r
+++ Gkountela2019/scran_results/pathway_analysis/Br_11/Up/combine_files.r
@@ -11,23 +11,38 @@ Br11_up <- load_data("~/Documents/Project/dataset/Gkountela2019/scran_results/pa
 
 Br11_up <- Br11_up[order(Br11_up$Adjusted.P.value,decreasing = F),]
 
+Br11_up_sig <- Br11_up[Br11_up$Adjusted.P.value < 0.05,]
+
 write.csv(Br11_up,'Br11_up_allPathways',row.names = F,quote = F)
+
+write.csv(Br11_up_sig,'Br11_up_SigPathways',row.names = F,quote = F)
 
 Br11_down <- load_data("~/Documents/Project/dataset/Gkountela2019/scran_results/pathway_analysis/Br_11/down/")
 
 Br11_down <- Br11_down[order(Br11_down$Adjusted.P.value,decreasing = F),]
 
+Br11_down_sig <- Br11_down[Br11_down$Adjusted.P.value < 0.05,]
+
 write.csv(Br11_down,'Br11_down_allPathways',row.names = F,quote = F)
 
+write.csv(Br11_down_sig,'Br11_down_SigPathways',row.names = F,quote = F)
 
 Br61_up <- load_data("~/Documents/Project/dataset/Gkountela2019/scran_results/pathway_analysis2/Br_61/up/")
 
 Br61_up <- Br61_up[order(Br61_up$Adjusted.P.value,decreasing = F),]
 
+Br61_up_sig <- Br61_up[Br61_up$Adjusted.P.value < 0.05,]
+
 write.csv(Br61_up,'../../../pathway_analysis2/Br_61/up/Br61_up_allPathways',row.names = F,quote = F)
+
+write.csv(Br61_up_sig,'../../../pathway_analysis2/Br_61/up/Br61_up_SigPathways',row.names = F,quote = F)
 
 Br61_down <- load_data("~/Documents/Project/dataset/Gkountela2019/scran_results/pathway_analysis2/Br_61/down/")
 
 Br61_down <- Br61_down[order(Br61_down$Adjusted.P.value,decreasing = F),]
 
+Br61_down_sig <- Br61_down[Br61_down$Adjusted.P.value < 0.05,]
+
 write.csv(Br11_down,'../../../pathway_analysis2/Br_61/down/Br61_down_allPathways',row.names = F,quote = F)
+
+write.csv(Br11_down_sig,'../../../pathway_analysis2/Br_61/down/Br61_down_SigPathways',row.names = F,quote = F)
