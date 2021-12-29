c285cf7b5c114fa6999a71b8b0f3c5d3c157ffc0
hmalavia <hmalavia2@gmail.com>
2021-12-29 15:49:10 +0530

    Common pathways script update

--- Gkountela2019/scran_results/pathway_analysis/Br_11/combine_epi_emt_files.r
+++ Gkountela2019/scran_results/pathway_analysis/Br_11/combine_epi_emt_files.r
@@ -52,24 +52,57 @@ Br11_emt_down_sig <- Br11_emt_down[Br11_emt_down$Adjusted.P.value < 0.05,]
 write.csv(Br11_emt_down,'Br11_emt_down_allPathways',row.names = F,quote = F)
 
 write.csv(Br11_emt_down_sig,'Br11_emt_down_SigPathways',row.names = F,quote = F)
-#### Br61 ####
+###### Br61 #######
 
-Br61_up <- load_data("~/Documents/Project/dataset/Gkountela2019/scran_results/pathway_analysis2/Br_61/up/")
+#### Epi genes ####
 
-Br61_up <- Br61_up[order(Br61_up$Adjusted.P.value,decreasing = F),]
+Br61_epi_up <- load_data("~/Documents/Project/dataset/Gkountela2019/scran_results/pathway_analysis2/Br_61/Epigenes_up_Br61/")
 
-Br61_up_sig <- Br61_up[Br61_up$Adjusted.P.value < 0.05,]
+Br61_epi_up <- Br61_epi_up[order(Br61_epi_up$Adjusted.P.value,decreasing = F),]
 
-write.csv(Br61_up,'../../../pathway_analysis2/Br_61/up/Br61_up_allPathways',row.names = F,quote = F)
+Br61_epi_up_sig <- Br61_epi_up[Br61_epi_up$Adjusted.P.value < 0.05,]
 
-write.csv(Br61_up_sig,'../../../pathway_analysis2/Br_61/up/Br61_up_SigPathways',row.names = F,quote = F)
+write.csv(Br61_epi_up,'../../../pathway_analysis2/Br_61/Epigenes_up_Br61/Br61_epi_up_allPathways',row.names = F,quote = F)
 
-Br61_down <- load_data("~/Documents/Project/dataset/Gkountela2019/scran_results/pathway_analysis2/Br_61/down/")
+write.csv(Br61_epi_up_sig,'../../../pathway_analysis2/Br_61/Epigenes_up_Br61/Br61_epi_up_SigPathways',row.names = F,quote = F)
 
-Br61_down <- Br61_down[order(Br61_down$Adjusted.P.value,decreasing = F),]
+#### EMT genes ####
+Br61_emt_up <- load_data("~/Documents/Project/dataset/Gkountela2019/scran_results/pathway_analysis2/Br_61/EMT_up_Br61/")
 
-Br61_down_sig <- Br61_down[Br61_down$Adjusted.P.value < 0.05,]
+Br61_emt_up <- Br61_emt_up[order(Br61_emt_up$Adjusted.P.value,decreasing = F),]
 
-write.csv(Br61_down,'../../../pathway_analysis2/Br_61/down/Br61_down_allPathways',row.names = F,quote = F)
+Br61_emt_up_sig <- Br61_emt_up[Br61_emt_up$Adjusted.P.value < 0.05,]
 
-write.csv(Br61_down_sig,'../../../pathway_analysis2/Br_61/down/Br61_down_SigPathways',row.names = F,quote = F)
+write.csv(Br61_emt_up,'../../../pathway_analysis2/Br_61/EMT_up_Br61/Br61_emt_up_AllPathways',row.names = F,quote = F)
+
+write.csv(Br61_emt_up_sig,'../../../pathway_analysis2/Br_61/EMT_up_Br61/Br61_emt_up_SigPathways',row.names = F,quote = F)
+
+Br61_emt_down <- load_data("~/Documents/Project/dataset/Gkountela2019/scran_results/pathway_analysis2/Br_61/EMT_down_Br61/")
+
+Br61_emt_down <- Br61_emt_down[order(Br61_emt_down$Adjusted.P.value,decreasing = F),]
+
+Br61_emt_down_sig <- Br61_emt_down[Br61_emt_down$Adjusted.P.value < 0.05,]
+
+write.csv(Br61_emt_down,'../../../pathway_analysis2/Br_61/EMT_down_Br61/Br61_emt_down_allPathways',row.names = F,quote = F)
+
+write.csv(Br61_emt_down_sig,'../../../pathway_analysis2/Br_61/EMT_down_Br61/Br61_emt_down_SigPathways',row.names = F,quote = F)
+
+
+#### Find Common Pathways ####
+
+Common_emt_up <- merge(Br11_emt_up_sig,Br61_emt_up_sig,by.x=1,by.y = 1)
+
+Common_emt_down <- merge(Br11_emt_down_sig,Br61_emt_down_sig,by.x=1,by.y=1)
+
+Common_epi_up <- merge(Br11_epi_up_sig,Br61_epi_up_sig,by.x = 1,by.y=1)
+
+Common_emt_up <- Common_emt_up[order(Common_emt_up$Combined.Score.x,decreasing = T),]
+
+Common_emt_down <- Common_emt_down[order(Common_emt_down$Combined.Score.x,decreasing = T),]
+
+Common_epi_up <- Common_epi_up[order(Common_epi_up$Combined.Score.x,decreasing = T),]
+
+
+write.csv(Common_emt_up,'../Common_emt_up_pathways.csv',row.names = F,quote = F)
+write.csv(Common_emt_down,'../Common_emt_down_pathways.csv',row.names = F,quote = F)
+write.csv(Common_epi_up,'../Common_epi_up_pathways.csv',row.names = F,quote = F)
